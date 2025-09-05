87c2887f73f6e6d51035eb4ebc4c4203704da264
Aryan Gupta <aryang60923@gmail.com>
2025-09-06 02:25:03 +0530

    updated

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,13 +1,10 @@
 import mongoose from "mongoose";
 
-export const connectDB =async()=>{
-    try{
-        const conn=await mongoose.connect(process.env.MONGODB_URI);
-        if(conn){
-            console.log("done");
-        }
-    }
-    catch(e){
-        console.log(e);
-    }
-}
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection error:", error);
+  }
+};
